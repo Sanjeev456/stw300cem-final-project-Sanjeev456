02e98406a5e23933e8592a17484eff8ee1fb4723
admin <gnyawali.sanjeev@gmail.com>
2019-05-07 00:06:00 +0545

    login,registration design and splash screen added

--- app/src/main/java/com/example/acos/MainActivity.java
+++ app/src/main/java/com/example/acos/MainActivity.java
@@ -9,11 +9,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etname,etpassword;
-    private Button btnlogin;
-    private TextView tvinfo;
-    private int counter=5;
 
+    private Button btnSignUp,btnSignActive;
 
 
     @Override
@@ -21,33 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etname=findViewById(R.id.etname);
-        etpassword=findViewById(R.id.etpassword);
-        btnlogin=findViewById(R.id.btnlogin);
-        tvinfo=findViewById(R.id.tvinfo);
-
-        tvinfo.setText("Number of attempts remaining:5");
-
-        btnlogin.setOnClickListener(new View.OnClickListener() {
+        btnSignUp=findViewById(R.id.btnSignUp);
+        btnSignActive=findViewById(R.id.btnSignActive);
+        btnSignActive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validate(etname.getText().toString(),etpassword.getText().toString());
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
             }
         });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+        startActivity(intent);
     }
-     private void validate(String username,String userpassword){
-        if((username.equals("sanjeev")) && (userpassword.equals("1234"))){
-            Intent intent=new Intent(MainActivity.this,Login.class);
-            startActivity(intent);
-        }
-        else{
-    counter--;
+});
 
-    tvinfo.setText("Number of attempts remaining:"+String.valueOf(counter));
-
-    if(counter==0){
-        btnlogin.setEnabled(false);
-    }
         }
      }
-}
+
